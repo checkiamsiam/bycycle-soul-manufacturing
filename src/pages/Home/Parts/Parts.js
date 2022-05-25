42bdd747474a80fd58974a024ebbc1bf9a7dd62e
checkiamsiam <issiam02415@gmail.com>
2022-05-25 10:01:37 +0600

    responsive

--- src/pages/Home/Parts/Parts.js
+++ src/pages/Home/Parts/Parts.js
@@ -9,23 +9,32 @@ const Parts = () => {
   return (
     <div className='container mx-auto py-5'>
       <h1 className='text-primary text-center text-4xl uppercase my-5'>Our Products</h1>
-      <div className='grid sm:grid-cols-2  grid-cols-1 gap-5'>
-        {data?.data?.map(p => <div class="card  bg-base-100 shadow-xl mx-3 relative h-screen">
-          <figure className=' h-1/3 md:h-1/2 overflow-hidden'><img src={p?.img} alt="Album" className='rounded w-full' h-full /></figure>
-          <div class="card-body">
-            <h2 class="card-title"><span className='font-semibold'>Name: {p?.name}</span></h2>
+      <div className='grid sm:grid-cols-2 lg:grid-cols-3 grid-cols-1 gap-5'>
+        {data?.data?.map(p => <div class=" h-screen mx-4 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700  overflow-hidden hover:shadow-orange-200">
+          <div className='overflow-hidden w-full h-1/2 '>
+            <img class="rounded-t-lg w-full h-full  transition duration-500 hover:scale-110" src={p?.img} alt="" />
+          </div>
+          <div class="p-5 ">
             <div>
-              <p><span className='font-semibold uppercase'>Available quantity: {p?.available}</span></p>
-              <p><span className='font-semibold uppercase'>minimum order: {p?.minOrdQnt}</span></p>
-              <p><span className='font-semibold uppercase'>price: {p?.price}</span></p>
-              <p><span className='text-sm'>{p?.description
-              }</span></p>
+              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{p?.name}</h5>
             </div>
-            <div class="card-actions  justify-center absolute bottom-5 ">
-              <button class="btn  rounded-md btn-sm md:btn bg-gradient-to-r from-primary to-secondary">BUY NOW</button>
+
+            <div className='flex flex-col justify-between'>
+              <div>
+                <p><span className='font-semibold uppercase'>Available quantity: {p?.available}</span></p>
+                <p><span className='font-semibold uppercase'>minimum order: {p?.minOrdQnt}</span></p>
+                <p><span className='font-semibold uppercase'>price: ${p?.price}</span></p>
+                <p><span className='font-semibold uppercase '>Description:</span> <span className='text-lg'>{p?.dis.slice(0, 100)}</span></p>
+              </div>
+
+              <div className=' w-full mt-5 '>
+                <button class="btn  rounded-md btn-sm md:btn bg-gradient-to-r from-primary to-secondary w-full">BUY NOW</button>
+              </div>
             </div>
+
           </div>
         </div>)}
+
 
       </div>
     </div>
